60067369f712ead50f8f7bad215a6f0a12c60f29
Chris Marshall <devel.chm.01@gmail.com>
2017-02-08 09:52:17 -0500

    Update VERSION to 0.01_09 for more development

--- lib/OpenGL/Modern.pm
+++ lib/OpenGL/Modern.pm
@@ -9,7 +9,7 @@ use Exporter 'import';
 use OpenGL::Modern::NameLists::Modern;
 use OpenGL::Modern::NameLists::MakefileAll;
 
-our $VERSION    = '0.01_08';
+our $VERSION    = '0.01_09';
 our $XS_VERSION = $VERSION;
 $VERSION = eval $VERSION;    # see L<perlmodstyle>
 
