79e4ba8c13bcda0cf4490b2e435520fe6b19e24b
Ed J <mohawk2@users.noreply.github.com>
2026-01-26 19:24:32 +0000

    for dynlang "<" param, allow "&" in front of other params

--- utils/common.pl
+++ utils/common.pl
@@ -107,7 +107,7 @@ sub bindings {
       my $val = delete $dynlang{$get};
       $val =~ s#^<##;
       my ($getfunc) = $val =~ /^(\w+)/;
-      $val =~ s#&#&$get#;
+      $val =~ s#&(?![a-z])#&$get#;
       my $vardata = $name2data{$get};
       $beforecall .= "  $vardata->[1]$get;\n  $val;\n";
       $this{error_check} .= "\n  " if $this{error_check};
